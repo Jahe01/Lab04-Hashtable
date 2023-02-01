629cc37abef2686be2ce21e879d08c60d6a364f7
Johanna Huaraca <86489770+Jahe01@users.noreply.github.com>
2023-02-01 00:53:08 -0500

    Add files via upload

---
+++ HashTable.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hashtableToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dictionaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortedDictionaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBoxFileInitial = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxFileFinal = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.textToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.closeFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "&Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // closeFileToolStripMenuItem
+            // 
+            this.closeFileToolStripMenuItem.Name = "closeFileToolStripMenuItem";
+            this.closeFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeFileToolStripMenuItem.Text = "&Close File";
+            this.closeFileToolStripMenuItem.Click += new System.EventHandler(this.closeFileToolStripMenuItem_Click);
+            // 
+            // textToolStripMenuItem
+            // 
+            this.textToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hashtableToolStripMenuItem1,
+            this.dictionaryToolStripMenuItem,
+            this.sortedDictionaryToolStripMenuItem});
+            this.textToolStripMenuItem.Name = "textToolStripMenuItem";
+            this.textToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.textToolStripMenuItem.Text = "Text";
+            // 
+            // hashtableToolStripMenuItem1
+            // 
+            this.hashtableToolStripMenuItem1.Name = "hashtableToolStripMenuItem1";
+            this.hashtableToolStripMenuItem1.Size = new System.Drawing.Size(165, 22);
+            this.hashtableToolStripMenuItem1.Text = "Hashtable";
+            this.hashtableToolStripMenuItem1.Click += new System.EventHandler(this.hashtableToolStripMenuItem1_Click);
+            // 
+            // dictionaryToolStripMenuItem
+            // 
+            this.dictionaryToolStripMenuItem.Name = "dictionaryToolStripMenuItem";
+            this.dictionaryToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.dictionaryToolStripMenuItem.Text = "Dictionary";
+            this.dictionaryToolStripMenuItem.Click += new System.EventHandler(this.dictionaryToolStripMenuItem_Click);
+            // 
+            // sortedDictionaryToolStripMenuItem
+            // 
+            this.sortedDictionaryToolStripMenuItem.Name = "sortedDictionaryToolStripMenuItem";
+            this.sortedDictionaryToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.sortedDictionaryToolStripMenuItem.Text = "Sorted Dictionary";
+            this.sortedDictionaryToolStripMenuItem.Click += new System.EventHandler(this.sortedDictionaryToolStripMenuItem_Click);
+            // 
+            // richTextBoxFileInitial
+            // 
+            this.richTextBoxFileInitial.Location = new System.Drawing.Point(25, 43);
+            this.richTextBoxFileInitial.Name = "richTextBoxFileInitial";
+            this.richTextBoxFileInitial.Size = new System.Drawing.Size(256, 353);
+            this.richTextBoxFileInitial.TabIndex = 2;
+            this.richTextBoxFileInitial.Text = "";
+            // 
+            // richTextBoxFileFinal
+            // 
+            this.richTextBoxFileFinal.Location = new System.Drawing.Point(470, 43);
+            this.richTextBoxFileFinal.Name = "richTextBoxFileFinal";
+            this.richTextBoxFileFinal.Size = new System.Drawing.Size(256, 353);
+            this.richTextBoxFileFinal.TabIndex = 3;
+            this.richTextBoxFileFinal.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check Spell -->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBoxFileFinal);
+            this.Controls.Add(this.richTextBoxFileInitial);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hashtable";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hashtableToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dictionaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortedDictionaryToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBoxFileInitial;
+        private System.Windows.Forms.RichTextBox richTextBoxFileFinal;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HashTable.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string filePath = string.Empty;
+        string dictionaryFile = string.Empty;
+        string targetFile = string.Empty;
+        string missedWordsFile = string.Empty;
+        public Form1()
+        {
+            dictionaryFile = ConfigurationManager.AppSettings["dictionaryFile"];
+            targetFile = ConfigurationManager.AppSettings["targetFile"];
+            missedWordsFile = ConfigurationManager.AppSettings["missedWordsFile"];
+            InitializeComponent();
+        }
+
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hashtableToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+            Hashtable englishHashDictionary = new Hashtable();
+            
+            DateTime startDate = DateTime.Now;
+            foreach (string line in System.IO.File.ReadLines(dictionaryFile))
+            {
+                if (!englishHashDictionary.ContainsKey(line))
+                {
+                    englishHashDictionary.Add(line, line);
+                }
+            }
+            TimeSpan timeDiff = DateTime.Now - startDate;
+            string result = string.Format("{0} add to Hashtable in {1} ms", dictionaryFile, timeDiff.TotalMilliseconds);
+            Console.WriteLine(result);
+            toolStripStatusLabel1.Text = result;
+            
+            IList<string> missedWords = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(targetFile))
+            {
+                string[] words = line.Replace(",", "").Replace("!", "").Split(' ');
+                foreach (string word in words)
+                {
+                    if (!englishHashDictionary.ContainsKey(word))
+                    {
+                        if (!missedWords.Contains(word))
+                        {
+                            missedWords.Add(word);
+                        }
+                    }
+                }
+            }
+            if (missedWords.Count > 0)
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, missedWords));
+                System.IO.File.WriteAllLines(missedWordsFile, missedWords);
+            }
+            Console.WriteLine("File processed!!");
+
+        }
+
+        private void dictionaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> englishDictionary = new Dictionary<string, string>();
+            
+            DateTime startDate = DateTime.Now;
+            foreach (string line in System.IO.File.ReadLines(dictionaryFile))
+            {
+                if (!englishDictionary.ContainsKey(line))
+                {
+                    englishDictionary.Add(line, line);
+                }
+            }
+            TimeSpan timeDiff = DateTime.Now - startDate;
+            string result = string.Format("{0} added to Dictionary in {1} ms", dictionaryFile, timeDiff.TotalMilliseconds);
+            Console.WriteLine(result);
+            toolStripStatusLabel2.Text = result;
+            
+            IList<string> missedWords = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(targetFile))
+            {
+                string[] words = line.Replace(",", "").Replace("!", "").Split(' ');
+                foreach (string word in words)
+                {
+                    if (!englishDictionary.ContainsKey(word))
+                    {
+                        if (!missedWords.Contains(word))
+                        {
+                            missedWords.Add(word);
+                        }
+                    }
+                }
+            }
+            if (missedWords.Count > 0)
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, missedWords));
+                System.IO.File.WriteAllLines(missedWordsFile, missedWords);
+            }
+            Console.WriteLine("File processed!!");
+        }
+
+        private void sortedDictionaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SortedDictionary<string, string> englishSortedDictionary = new SortedDictionary<string, string>();
+           
+            DateTime startDate = DateTime.Now;
+            foreach (string line in System.IO.File.ReadLines(dictionaryFile))
+            {
+                if (!englishSortedDictionary.ContainsKey(line))
+                {
+                    englishSortedDictionary.Add(line, line);
+                }
+            }
+            TimeSpan timeDiff = DateTime.Now - startDate;
+            string result = string.Format("{0} added to SortedDictionary in {1} ms", dictionaryFile, timeDiff.TotalMilliseconds);
+            Console.WriteLine(result);
+            toolStripStatusLabel3.Text = result;
+            
+            IList<string> missedWords = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(targetFile))
+            {
+                string[] words = line.Replace(",", "").Replace("!", "").Split(' ');
+                foreach (string word in words)
+                {
+                    if (!englishSortedDictionary.ContainsKey(word))
+                    {
+                        if (!missedWords.Contains(word))
+                        {
+                            missedWords.Add(word);
+                        }
+                    }
+                }
+            }
+            if (missedWords.Count > 0)
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, missedWords));
+                System.IO.File.WriteAllLines(missedWordsFile, missedWords);
+            }
+            Console.WriteLine("File processed!!");
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    filePath = openFileDialog.FileName;
+                    try
+                    {
+                        richTextBoxFileInitial.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar el archivo: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void closeFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBoxFileInitial.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hashtable englishHashDictionary = new Hashtable();
+            
+            foreach (string line in System.IO.File.ReadLines(dictionaryFile))
+            {
+                if (!englishHashDictionary.ContainsKey(line))
+                {
+                    englishHashDictionary.Add(line, line);
+                }
+            }
+            
+            string[] lines = richTextBoxFileInitial.Text.Split('\n');
+            richTextBoxFileFinal.Clear();
+            foreach (string line in lines)
+            {
+                string[] words = line.Replace(",", "").Replace("!", "").Split(' ');
+                foreach (string word in words)
+                {
+                    if (!englishHashDictionary.ContainsKey(word))
+                    {
+                        richTextBoxFileFinal.SelectionColor = Color.Red;
+                    }
+                    else
+                    {
+                        richTextBoxFileFinal.SelectionColor = richTextBoxFileInitial.SelectionColor;
+                    }
+                    richTextBoxFileFinal.AppendText(word + " ");
+                }
+                richTextBoxFileFinal.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
